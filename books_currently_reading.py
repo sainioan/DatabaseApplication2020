d28e55afa661773331445c2492bc9e45715732a9
sainioan <sainio.anniina@gmail.com>
2020-08-13 22:24:52 +0300

    reading percentage

--- books_currently_reading.py
+++ books_currently_reading.py
@@ -16,7 +16,7 @@ def new_book(title, author, genre, plot_summary, current_page, pages, user_id):
 
 
 def show(user_id):
-    sql = "SELECT * FROM books_currently_reading WHERE " \
+    sql = "SELECT book_id, title, author, genre, plot_summary,current_page, pages, user_id,  round(cast(current_page::float / pages::float as numeric), 2) AS percentage FROM books_currently_reading WHERE " \
           "user_id=:user_id ORDER BY title ASC "
     result = db.session.execute(sql, {"user_id": user_id})
     db.session.commit()
@@ -41,14 +41,7 @@ def transfer_to_books_read(user_id, book_id):
     return True
 
 
-#
-# def show_percentage(user_id):
-#     sql = "SELECT current_page,pages, count(current_page)* 100/ pages as percentage  FROM books_currently_reading " \
-#           "WHERE user_id=:user_id"
-#     result = db.session.execute(sql, {"user_id": user_id})
-#     db.session.commit()
-#     my_current_book_list = result.fetchall()
-#     return my_current_book_list
+
 
 
 def update_pageNumber(user_id, current_page):
